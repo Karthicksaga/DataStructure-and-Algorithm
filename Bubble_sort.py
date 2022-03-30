e2e0b6550d2b538f5f24bc9b01347f96b5f6d2fa
Karthick T J <61616267+Karthicksaga@users.noreply.github.com>
2022-03-30 20:46:37 +0530

    Update Bubble_sort.py

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -1,17 +1,19 @@
 def bubble_sort(array):
-    
     #iterate the Main array
     #Iterate the whole pass
     for i in range(len(array)):
-        
         for j in range(0,len(array) - i - 1):
-            
             if array[j] > array[j + 1]:
-                
                 array[j],array[j + 1] = array[j + 1],array[j]
-                
             print("Inner Loop Executed Succefully {0}".format(j))
             
     return array
 array = [441,262,19,519,476,758,359,140]
 print(bubble_sort(array))
+
+##Time Complexity : O(n * n)
+#Space Complexity : 0(n)
+#best Case: O(n) - when array is Already sorted
+## worst Case : O(n*2)
+
+#Where it can be used : Computer Graphics
